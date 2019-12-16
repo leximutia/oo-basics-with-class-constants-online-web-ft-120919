c4a5ba4384a4691a483da3a7e50cdcd9be11ef47
Lexi Mutia <muse.malize@gmail.com>
2019-12-16 17:18:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,9 +2,9 @@ class Shoe
 
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
+
   BRANDS = []
-  
+
   def initialize(brand)
     @brand = brand
     BRANDS << brand unless BRANDS.include?(brand)
@@ -14,5 +14,5 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
+
 end
